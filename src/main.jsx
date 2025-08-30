7dc8a9eae0a0e3ec90aef5b9530f4f331af73022
John Turbitt <John.Turbitt@betbright.com>
2025-08-30 14:53:37 +0100

    Add templates for Cookies | Adds | Buymeacoffee

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import MortgageCalculator from './MortgageCalculator'
+import Extras from './Extras'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <MortgageCalculator />
+    <Extras />
   </StrictMode>,
 )
